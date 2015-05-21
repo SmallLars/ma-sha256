e41d8a1884427600900c3ed1074c38df93ae4ae2
Lars Schmertmann <lars@tzi.de>
2015-05-21 11:02:13 +0200

    logfix

--- knf_gen/knf_gen.cpp
+++ knf_gen/knf_gen.cpp
@@ -8,12 +8,15 @@ int main()
 {
     SATSolver solver;
     vector<Lit> clause;
+    vector<unsigned> x_clause;
+
+    solver.log_to_file("mylog.txt");
 
     //Let's use 4 threads
     solver.set_num_threads(4);
 
     //We need 3 variables
-    solver.new_vars(3);
+    solver.new_vars(6);
 
     //adds "1 0"
     clause.push_back(Lit(0, false));
@@ -31,11 +34,20 @@ int main()
     clause.push_back(Lit(2, false));
     solver.add_clause(clause);
 
+    //xortest
+    x_clause.clear();
+    x_clause.push_back(3);
+    x_clause.push_back(4);
+    x_clause.push_back(5);
+    solver.add_xor_clause(x_clause, true);
+
     lbool ret = solver.solve();
+/*
     assert(ret == l_True);
     assert(solver.get_model()[0] == l_True);
     assert(solver.get_model()[1] == l_False);
     assert(solver.get_model()[2] == l_True);
+*/
     std::cout
     << "Solution is: "
     << solver.get_model()[0]
